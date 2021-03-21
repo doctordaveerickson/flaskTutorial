c975a8862a092881c3c7e4f17e06893f10af0829
Dave Erickson <dave@ericksononline.net>
2021-03-21 09:01:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,108 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask import Flask, request, jsonify, make_response, g
+from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
+import yaml
+
+app = Flask(__name__)
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(100), nullable=False)
+	views = db.Column(db.Integer, nullable=False)
+	likes = db.Column(db.Integer, nullable=False)
+
+	def __repr__(self):
+		return f"Video(name = {name}, views = {views}, likes = {likes})"
+
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
+
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video")
+video_update_args.add_argument("likes", type=int, help="Likes on the video")
+
+resource_fields = {
+	'id': fields.Integer,
+	'name': fields.String,
+	'views': fields.Integer,
+	'likes': fields.Integer
+}
+
+class Video(Resource):
+	@marshal_with(resource_fields)
+	def get(self, video_id):
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Could not find video with that id")
+		return result
+
+	@marshal_with(resource_fields)
+	def put(self, video_id):
+		args = video_put_args.parse_args()
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if result:
+			abort(409, message="Video id taken...")
+
+		video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+		db.session.add(video)
+		db.session.commit()
+		return video, 201
+
+	@marshal_with(resource_fields)
+	def patch(self, video_id):
+		args = video_update_args.parse_args()
+		result = VideoModel.query.filter_by(id=video_id).first()
+		if not result:
+			abort(404, message="Video doesn't exist, cannot update")
+
+		if args['name']:
+			result.name = args['name']
+		if args['views']:
+			result.views = args['views']
+		if args['likes']:
+			result.likes = args['likes']
+
+		db.session.commit()
+
+		return result
+
+
+	def delete(self, video_id):
+		abort_if_video_id_doesnt_exist(video_id)
+		del videos[video_id]
+		return '', 204
+
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+SWAGGER_URL = "/ehegenapi/v1/docs"  # URL for exposing Swagger UI (without trailing '/')
+API_URL = "/ehegenapi/v1/openapi.json"
+
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={"app_name": "EasternHops E-Generator API"},  # Swagger UI config overrides
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+# @app.route("/ehegenapi/v1/openapi.json", methods=["GET"])
+# def get_open_api_spec():
+#     with open("openapi.yaml", "r") as r:
+#         openapi = yaml.safe_load(r)
+#     return jsonify(openapi)
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
